b0874cfd220340aba45974c0ddde986ba34ca0c5
khuzi-shaikh <124673301+khuzi-shaikh@users.noreply.github.com>
2023-02-25 17:50:19 +0530

    E-Comm Project

---
+++ src/Route/Auth.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+
+const Auth = () => {
+    const userLogged = false;
+  return (
+    <div>
+    {userLogged ? <PrivateRoute/> : <PublicRoute/>}
+    </div>
+  )
+}
+
+export default Auth;
